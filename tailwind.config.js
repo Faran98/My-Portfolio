e16e77b44c3a309587b8b6d770d371dd4672c2fe
faran98 <farankhalil98@gmail.com>
2024-07-23 20:32:01 +0500

    Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
       'custom-gradient': 'linear-gradient(to top right, rgba(255, 165, 0, 0) 45%, rgba(255, 165, 0, 1) 100%)',
     },
     colors: {
-      'nav-color': 'rgb(146, 78, 209)',
+      'nav-color': 'rgb(146, 78, 209)', 
+      'nav-white':'rgb(0,0,0,0)',
+      'nav-black':'rgb(0,0,0)',
     },
   },
   plugins: [],
